82676b67c0071443fa8e7a9cc583bcbb2649adf0
kirankumars1986 <sivakotikirankumar@gmail.com>
2020-07-03 12:38:23 -0500

    Deleted ID

--- src/main/java/com/likki/bank/controller/LoginController.java
+++ src/main/java/com/likki/bank/controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * @author kks10 - kiran
+ * @author kks10
  *
  */
 @Controller
